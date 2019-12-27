1746452570ded2921a3489aa27c0c61d4200cca3
Cristi <cristi.dnla@gmail.com>
2019-12-27 15:30:55 +0200

    Add empty spring boot service template for knowledge service

---
+++ knowledge-store-service/src/main/java/com/wd/knowledge/KnowledgeStoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wd.knowledge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeStoreServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowledgeStoreServiceApplication.class, args);
+    }
+
+}
---
+++ knowledge-store-service/src/test/java/com/wd/knowledge/KnowledgeStoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wd.knowledge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeStoreServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
