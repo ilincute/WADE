442ea7e8c3d4ae127cd1fc2a0d5bb955d7246863
Cristi <cristi.dnla@gmail.com>
2020-01-20 23:09:10 +0200

    Add project template for the registration and authentication service

---
+++ raservice/src/main/java/com/regauth/raservice/RaserviceApplication.java
@@ -0,0 +1,13 @@
+package com.regauth.raservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaserviceApplication.class, args);
+    }
+
+}
