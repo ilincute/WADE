1cb87fdf4e9e4e840cf7fe7f2437c895fe25077d
Cristi <cristi.dnla@gmail.com>
2020-01-28 15:51:09 +0200

    Add sample controller

--- raservice/src/main/java/com/regauth/raservice/LoginController.java
+++ raservice/src/main/java/com/regauth/raservice/LoginController.java
@@ -1,4 +1,34 @@
 package com.regauth.raservice;
 
-public class LoginController {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/ra")
+public class LoginController
+{
+    @PostMapping("/register")
+    public String tryRegister(
+            @RequestParam(value = "userName") String userName,
+            @RequestParam(value = "lastName") String lastName,
+            @RequestParam(value = "email") String email,
+            @RequestParam(value = "password") String password)
+    {
+        System.out.println(userName);
+        System.out.println(lastName);
+        System.out.println(email);
+        System.out.println(password);
+
+        return "hello";
+    }
+
+    @RequestMapping(path = "/sayHello", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> hello()
+    {
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
 }
