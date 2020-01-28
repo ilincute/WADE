091b15e9375ac0b00a4305f2c916ef0a574bbfed
Cristian Danila <marius.cristian.danila@sap.com>
2020-01-28 21:58:21 +0200

    Add method for delete in TokenDAO

--- coreapi/src/main/java/coreapi/coreapi/dao/TokenDAO.java
+++ coreapi/src/main/java/coreapi/coreapi/dao/TokenDAO.java
@@ -11,6 +11,7 @@ public class TokenDAO
 {
 
     public static final String INSERT_FORMAT = "insert into tokens values (?, ?)";
+    public static final String DELETE_FORMAT = "delete from tokens where token = ?";
 
     public static String addForUser(String userId)
     {
@@ -44,9 +45,37 @@ public class TokenDAO
                 System.err.println(e.getMessage());
             }
         }
-
-
         return token;
     }
 
+
+    public static void delete(String token)
+    {
+        Connection con = null;
+        try
+        {
+            con = DBFactory.getConnection();
+
+            PreparedStatement statement = con.prepareStatement(DELETE_FORMAT);
+            statement.setString(1, token);
+
+            statement.executeUpdate();
+        }
+        catch (SQLException e)
+        {
+
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                if (con != null) con.close();
+            }
+            catch(SQLException e)
+            {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
 }
--- coreapi/src/test/java/coreapi/coreapi/TokenDAOTest.java
+++ coreapi/src/test/java/coreapi/coreapi/TokenDAOTest.java
@@ -4,18 +4,31 @@ import coreapi.coreapi.dao.TokenDAO;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @SpringBootTest
 public class TokenDAOTest
 {
-
     @Test
     void testCreate()
     {
-
-
         assertNotEquals(null, TokenDAO.addForUser("1234234"));
+    }
 
+    @Test
+    void testDelete()
+    {
+        Exception ex = null;
+        try
+        {
+            TokenDAO.delete("9cb555f0-3afa-4b75-97b1-05d43d9451fd");
+        }
+        catch (Exception e)
+        {
+            ex = e;
+        }
+
+        assertEquals(null, ex);
     }
 }
