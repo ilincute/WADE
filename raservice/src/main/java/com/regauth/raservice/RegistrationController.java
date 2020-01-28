9737acf23c7654d57eff47ee405fc7e3d9f647f2
Cristian Danila <marius.cristian.danila@sap.com>
2020-01-28 17:44:39 +0200

    Implement basic registration endpoint

--- raservice/src/main/java/com/regauth/raservice/RegistrationController.java
+++ raservice/src/main/java/com/regauth/raservice/RegistrationController.java
@@ -1,4 +1,40 @@
 package com.regauth.raservice;
 
-public class RegistrationController {
+import com.regauth.facades.UserFacade;
+import com.regauth.validators.UserValidator;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.awt.*;
+
+@Controller
+@RequestMapping("/ra")
+public class RegistrationController
+{
+
+    @RequestMapping(path = "/register", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseEntity<String> register(
+            @RequestParam(name = "userName") String userName,
+            @RequestParam(name = "lastName") String lastName,
+            @RequestParam(name = "email") String email,
+            @RequestParam(name = "password") String password)
+    {
+
+        try
+        {
+            UserFacade.register(userName, lastName, email, password);
+            return new ResponseEntity<>("", HttpStatus.CREATED);
+        }
+        catch (Exception e)
+        { }
+
+        return new ResponseEntity<>("", HttpStatus.CONFLICT);
+    }
 }
