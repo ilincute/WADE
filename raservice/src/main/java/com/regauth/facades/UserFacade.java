78ba3cd6b05890a735817a866d0c9cf8c61f5838
Cristian Danila <marius.cristian.danila@sap.com>
2020-01-28 17:43:24 +0200

    Add user facade

---
+++ raservice/src/main/java/com/regauth/facades/UserFacade.java
@@ -0,0 +1,24 @@
+package com.regauth.facades;
+
+import com.regauth.dao.UserDAO;
+import com.regauth.data.User;
+import org.apache.commons.codec.digest.DigestUtils;
+
+import java.util.UUID;
+
+public class UserFacade
+{
+    public static void register(String userName, String lastName, String email, String password) throws Exception
+    {
+        User user = new User();
+
+        user.setId(UUID.randomUUID().toString());
+        user.setPassword(DigestUtils.sha256Hex(password));
+        user.setUserName(userName);
+        user.setEmail(email);
+        user.setLastName(lastName);
+
+        UserDAO.create(user);
+    }
+
+}
