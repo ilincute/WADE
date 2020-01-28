f7b12a556833b672e014596446c5750f59a4692b
Cristi <cristi.dnla@gmail.com>
2020-01-28 15:00:50 +0200

    Add test method for get in UserDataDAO

--- raservice/src/test/java/com/regauth/raservice/UserDataDAOTest.java
+++ raservice/src/test/java/com/regauth/raservice/UserDataDAOTest.java
@@ -1,11 +1,15 @@
 package com.regauth.raservice;
 
 import com.regauth.dao.UserDataDAO;
+import com.regauth.data.User;
 import com.regauth.data.UserData;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @SpringBootTest
 public class UserDataDAOTest
@@ -57,6 +61,16 @@ public class UserDataDAOTest
 
         assertEquals(null, ex);
 
+    }
+
+    @Test
+    void testGet()
+    {
+        User user = new User();
+        user.setId("321");
+        List<UserData> metadatas = UserDataDAO.getMetadataFor(user);
+
+        assertNotEquals(null, metadatas);
     }
 
 }
