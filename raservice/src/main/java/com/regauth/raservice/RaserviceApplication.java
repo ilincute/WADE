b40d61ab55afd394d3b4e7de1bae1e3f95dd8dea
Cristian Danila <marius.cristian.danila@sap.com>
2020-01-28 23:39:27 +0200

    Add service layer to query `raservice`

--- raservice/src/main/java/com/regauth/raservice/RaserviceApplication.java
+++ raservice/src/main/java/com/regauth/raservice/RaserviceApplication.java
@@ -2,6 +2,7 @@ package com.regauth.raservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class RaserviceApplication {
