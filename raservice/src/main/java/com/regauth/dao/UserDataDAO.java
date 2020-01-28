363e4a53221be6e7ad4370553b07b9cb38307d0d
Cristi <cristi.dnla@gmail.com>
2020-01-28 14:51:28 +0200

    Implement update method in UserDataDAO

--- raservice/src/main/java/com/regauth/dao/UserDataDAO.java
+++ raservice/src/main/java/com/regauth/dao/UserDataDAO.java
@@ -12,7 +12,13 @@ public class UserDataDAO
 {
 
     private static final String INSERT_FORMAT = "insert into userdata values(?, ?, ?, ?)";
+    private static final String UPDATE_FORMAT
+            = "update userdata set userId = ?, actionType = ?, actionValue = ? where id = ?";
 
+    private static final String USER_DATA_ATTR_ID = "id";
+    private static final String USER_DATA_ATTR_USER_ID = "userId";
+    private static final String USER_DATA_ATTR_ACTION_TYPE = "actionType";
+    private static final String USER_DATA_ATTR_ACTION_VAUE = "actionValue";
 
     public static void create(UserData metadata)
     {
@@ -54,6 +60,38 @@ public class UserDataDAO
 
     public static void update(UserData metadata)
     {
+        Connection con = null;
+        try
+        {
+            con = DBFactory.getConnection();
+
+            PreparedStatement statement = con.prepareStatement(UPDATE_FORMAT);
+            statement.setQueryTimeout(30);
+
+            statement.setString(1, metadata.getUserId());
+            statement.setString(2, metadata.getActionType());
+            statement.setString(3, metadata.getActionValue());
+            statement.setString(4, metadata.getId());
+
+            statement.executeUpdate();
+        }
+        catch (SQLException e)
+        {
+
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                if (con != null) con.close();
+            }
+            catch(SQLException e)
+            {
+                System.err.println(e.getMessage());
+            }
+        }
+
 
     }
 
