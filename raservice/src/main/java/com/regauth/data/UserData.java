712914c8ed2763cf9640f12e771781eb3d78bbb8
Cristi <cristi.dnla@gmail.com>
2020-01-28 11:09:44 +0200

    Add UserData data object

---
+++ raservice/src/main/java/com/regauth/data/UserData.java
@@ -0,0 +1,40 @@
+package com.regauth.data;
+
+public class UserData {
+    private String id;
+    private String userId;
+    private String actionType;
+    private String actionValue;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getActionType() {
+        return actionType;
+    }
+
+    public void setActionType(String actionType) {
+        this.actionType = actionType;
+    }
+
+    public String getActionValue() {
+        return actionValue;
+    }
+
+    public void setActionValue(String actionValue) {
+        this.actionValue = actionValue;
+    }
+}
