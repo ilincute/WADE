74cbe324909f6d35707faca0c64062434c610c71
Cristi <cristi.dnla@gmail.com>
2020-01-28 14:38:13 +0200

    Add test for create method

--- raservice/src/test/java/com/regauth/raservice/UserDataDAOTest.java
+++ raservice/src/test/java/com/regauth/raservice/UserDataDAOTest.java
@@ -1,7 +1,37 @@
 package com.regauth.raservice;
 
+import com.regauth.dao.UserDataDAO;
+import com.regauth.data.UserData;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class UserDataDAOTest {
+
+    @Test
+    void testCreate()
+    {
+        Exception ex = null;
+        try
+        {
+            UserData metadata = new UserData();
+            metadata.setId("123");
+            metadata.setUserId("321");
+            metadata.setActionType("action");
+            metadata.setActionValue("value");
+
+            UserDataDAO.create(metadata);
+        }
+        catch (Exception e)
+        {
+            ex = e;
+        }
+
+        assertEquals(null, ex);
+
+
+    }
+
 }
