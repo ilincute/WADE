c5d3ffbdf27be74f1d6881f48e4e621fb35f110f
Cristi <cristi.dnla@gmail.com>
2020-01-28 13:33:46 +0200

    Complete create method for user DAO

--- raservice/src/main/java/com/regauth/dao/UserDAO.java
+++ raservice/src/main/java/com/regauth/dao/UserDAO.java
@@ -1,11 +1,51 @@
 package com.regauth.dao;
 
 import com.regauth.data.User;
+import com.regauth.dbcore.DBFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class UserDAO
 {
+    private static final String INSERT_FORMAT = "insert into user values(?, ?, ?, ?, ?)";
+
     public static void create(User user)
     {
+        Connection con = null;
+        try
+        {
+            con = DBFactory.getConnection();
+
+            PreparedStatement statement = con.prepareStatement(INSERT_FORMAT);
+            statement.setQueryTimeout(30);
+            statement.setString(1, user.getId());
+            statement.setString(2, user.getUserName());
+            statement.setString(3, user.getLastName());
+            statement.setString(4, user.getEmail());
+            statement.setString(5, user.getPassword());
+
+            statement.executeUpdate();
+        }
+        catch (SQLException e)
+        {
+
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                if(con != null)
+                    con.close();
+            }
+            catch(SQLException e)
+            {
+                System.err.println(e.getMessage());
+            }
+        }
 
     }
 
