ea9a87e87c93604f55db3875f8896b1e71afbcf9
Cristi <cristi.dnla@gmail.com>
2020-01-28 12:35:57 +0200

    Add stub methods for the user DAO

---
+++ raservice/src/main/java/com/regauth/dao/UserDAO.java
@@ -0,0 +1,22 @@
+package com.regauth.dao;
+
+import com.regauth.data.User;
+
+public class UserDAO
+{
+    public static void create(User user)
+    {
+
+    }
+
+
+    public static User retrieve()
+    {
+        return null;
+    }
+
+    public static void update(User user)
+    {
+
+    }
+}
