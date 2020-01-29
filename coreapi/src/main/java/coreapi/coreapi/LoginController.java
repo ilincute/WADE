941fb8eb9616feb994e4721c68ecae330feb8b2d
cristid9 <cristi.dnla@gmail.com>
2020-01-29 13:07:22 +0200

    Enable `Access-Control-Allow-Origin` headers

--- coreapi/src/main/java/coreapi/coreapi/LoginController.java
+++ coreapi/src/main/java/coreapi/coreapi/LoginController.java
@@ -6,16 +6,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/core")
 public class LoginController
 {
 
+    @CrossOrigin(origins = "*")
     @RequestMapping(path = "/authenticate", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public ResponseEntity<String> login(
--- coreapi/src/main/java/coreapi/coreapi/TokenController.java
+++ coreapi/src/main/java/coreapi/coreapi/TokenController.java
@@ -5,10 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/core")
@@ -17,6 +14,7 @@ public class TokenController
     public static final String VALID = "valid";
     public static final String INVALID = "invalid";
 
+    @CrossOrigin(origins = "*")
     @RequestMapping(path = "/check", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public ResponseEntity<String> checkValidToken(@RequestParam(name = "token") String token)
